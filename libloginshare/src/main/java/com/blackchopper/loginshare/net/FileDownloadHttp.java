da8eade7c5a812c8179e657ff21f67389f692eb1
Black Chopper <4884280@qq.com>
2018-07-04 17:37:18 +0800

    Update FileDownloadHttp.java

--- libloginshare/src/main/java/com/blackchopper/loginshare/net/FileDownloadHttp.java
+++ libloginshare/src/main/java/com/blackchopper/loginshare/net/FileDownloadHttp.java
@@ -25,74 +25,57 @@ public abstract class FileDownloadHttp {
         this.mContext = context;
     }
 
-    public static final byte[] input2byte(InputStream inStream)
-            throws IOException {
-        ByteArrayOutputStream swapStream = new ByteArrayOutputStream();
-        byte[] buff = new byte[100];
-        int rc = 0;
-        while ((rc = inStream.read(buff, 0, 100)) > 0) {
-            swapStream.write(buff, 0, rc);
-        }
-        byte[] in2b = swapStream.toByteArray();
-        return in2b;
-    }
-
-    public static File writeBytesToFile(byte[] b, String outputFile) {
-        File file = null;
-        FileOutputStream os = null;
-
-        try {
-            file = new File(outputFile);
-            os = new FileOutputStream(file);
-            os.write(b);
-        } catch (Exception var13) {
-            var13.printStackTrace();
-        } finally {
-            try {
-                if (os != null) {
-                    os.close();
-                }
-            } catch (IOException var12) {
-                var12.printStackTrace();
-            }
-
-        }
-
-        return file;
-    }
-
+   
     public void executor() {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                FileOutputStream fos = null;
                 try {
                     URL url = new URL(mUrl);
-                    HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-                    httpConn.setDoOutput(true);// 使用 URL 连接进行输出
-                    httpConn.setDoInput(true);// 使用 URL 连接进行输入
-                    httpConn.setUseCaches(false);// 忽略缓存
-                    httpConn.setRequestMethod("GET");// 设置URL请求方法
-                    //可设置请求头
-                    httpConn.setRequestProperty("Content-Type", "application/octet-stream");
-                    httpConn.setRequestProperty("Connection", "Keep-Alive");// 维持长连接
-                    httpConn.setRequestProperty("Charset", "UTF-8");
-                    byte[] file = input2byte(httpConn.getInputStream());
-                    String path = Environment.getExternalStorageDirectory().getPath() + "Android/data/";
-                    String packageName = mContext.getPackageName();
-                    path = path + packageName.replace(".", "/") + "/loginshare/";
-                    new File(path).mkdirs();
-                    path = path + System.currentTimeMillis() + ".jpg";
-                    writeBytesToFile(file, path);
-                    Log.i(FileDownloadHttp.this.getClass().getName(), path);
-                    final String finalPath = path;
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            onResult(finalPath);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(3000);
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        String path = Environment.getExternalStorageDirectory().getPath() + "/Android/data/";
+                        String packageName = mContext.getPackageName();
+                        path = path + packageName.replace(".", "/") + "/loginshare/";
+                        new File(path).mkdirs();
+                        Log.i(FileDownloadHttp.class.getName(), path);
+                        path = path + System.currentTimeMillis() + ".jpg";
+                        Log.i(FileDownloadHttp.class.getName(), path);
+                        InputStream is = conn.getInputStream();
+                        fos = new FileOutputStream(new File(path));
+                        byte[] b = new byte[1024];
+                        int len = 0;
+                        while ((len = is.read(b)) != -1) {
+                            fos.write(b, 0, len);
                         }
-                    });
+                        fos.flush();
+                        final String finalPath = path;
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                onResult(finalPath);
+                            }
+                        });
+                    } else {
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                onError();
+                            }
+                        });
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
+                } finally {
+                    if (fos != null) {
+                        try {
+                            fos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                     mHandler.post(new Runnable() {
                         @Override
                         public void run() {
@@ -100,7 +83,6 @@ public abstract class FileDownloadHttp {
                         }
                     });
                 }
-
             }
         }).start();
 
